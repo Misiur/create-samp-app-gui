3d17e28b918ecb7bef59b01a1070a91d983ffccd
Misiur <misiur66+github@gmail.com>
2017-08-16 10:50:05 +0200

    Fix annoying bug (node_modules has to be in webpack config)

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
       ],
     }, {
       test: /\.js$/,
-      include: path.join(__dirname, 'src'),
+      include: path.join(__dirname),
       use: ['babel-loader'],
     }],
   },
